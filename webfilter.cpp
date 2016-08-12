dbfbe60890fa6c79d3d30f361d1be8b47db28ccf
kminchul <k.minchul95@gmail.com>
2016-08-12 20:19:34 +0900

    domain 기반 차단에 불필요한 부분 삭제. BlackListMatch 함수 제작을 남겨놓은 상태

--- webfilter.cpp
+++ webfilter.cpp
@@ -1,6 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 /*
-* webfilter.c
+* webfilter.c -> webfilter.cpp
 * (C) 2014, all rights reserved,
 *
 * This program is free software: you can redistribute it and/or modify
@@ -37,21 +37,22 @@
 #define MAXBUF 0xFFFF
 #define MAXURL 4096
 
+#define MAX_URL_LEN 100
+#define MAX_BLACKLIST_LEN 10000
+
 /*
 * URL and blacklist representation.
 */
-typedef struct
-{
-	char *domain;
-	char *uri;
-} URL, *PURL;
 
-typedef struct
+struct URL {
+	char domain[MAX_URL_LEN+1];
+};
+
+struct URLlist
 {
-	UINT size;
-	UINT length;
-	PURL *urls;
-} BLACKLIST, *PBLACKLIST;
+	int len;
+	URL list[MAX_BLACKLIST_LEN];
+};
 
 /*
 * Pre-fabricated packets.
@@ -91,15 +92,11 @@ const char block_data[] =
 * Prototypes
 */
 static void PacketInit(PPACKET packet);
-static int __cdecl UrlCompare(const void *a, const void *b);
-static int UrlMatch(PURL urla, PURL urlb);
-static PBLACKLIST BlackListInit(void);
-static void BlackListInsert(PBLACKLIST blacklist, PURL url);
-static void BlackListSort(PBLACKLIST blacklist);
-static BOOL BlackListMatch(PBLACKLIST blacklist, PURL url);
-static void BlackListRead(PBLACKLIST blacklist, const char *filename);
-static BOOL BlackListPayloadMatch(PBLACKLIST blacklist, char *data,
-	UINT16 len);
+static BOOL BlackListMatch(char *domain);
+static void BlackListRead(const char *filename);
+static BOOL BlackListPayloadMatch(char *data, UINT16 len);
+
+URLlist blacklist; // due to memory size
 
 /*
 * Entry.
@@ -120,23 +117,23 @@ int __cdecl main(int argc, char **argv)
 	PPACKET finish = &finish0;
 	PDATAPACKET blockpage;
 	UINT16 blockpage_len;
-	PBLACKLIST blacklist;
-	unsigned i;
+
 	INT16 priority = 404;       // Arbitrary.
 
 								// Read the blacklists.
 	if (argc <= 1)
 	{
-		fprintf(stderr, "usage: %s blacklist.txt [blacklist2.txt ...]\n",
-			argv[0]);
+		fprintf(stderr, "usage: %s blacklist.txt\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
-	blacklist = BlackListInit();
-	for (i = 1; i < (UINT)argc; i++)
-	{
-		BlackListRead(blacklist, argv[i]);
-	}
-	BlackListSort(blacklist);
+
+	// init blacklist, actually not necessary
+	blacklist.len = 0;
+	// read blacklist
+	BlackListRead(argv[1]);
+	// pre-processing
+
+
 
 	// Initialize the pre-frabricated packets:
 	blockpage_len = sizeof(DATAPACKET) + sizeof(block_data) - 1;
@@ -187,7 +184,7 @@ int __cdecl main(int argc, char **argv)
 
 		if (!WinDivertHelperParsePacket(packet, packet_len, &ip_header, NULL,
 			NULL, NULL, &tcp_header, NULL, &payload, &payload_len) ||
-			!BlackListPayloadMatch(blacklist, (char*)payload, (UINT16)payload_len))
+			!BlackListPayloadMatch((char*)payload, (UINT16)payload_len))
 		{
 			// Packet does not match the blacklist; simply reinject it.
 			if (!WinDivertSend(handle, packet, packet_len, &addr, NULL))
@@ -266,102 +263,22 @@ static void PacketInit(PPACKET packet)
 }
 
 /*
-* Initialize an empty blacklist.
-*/
-static PBLACKLIST BlackListInit(void)
-{
-	PBLACKLIST blacklist = (PBLACKLIST)malloc(sizeof(BLACKLIST));
-	UINT size;
-	if (blacklist == NULL)
-	{
-		goto memory_error;
-	}
-	size = 1024;
-	blacklist->urls = (PURL *)malloc(size * sizeof(PURL));
-	if (blacklist->urls == NULL)
-	{
-		goto memory_error;
-	}
-	blacklist->size = size;
-	blacklist->length = 0;
-
-	return blacklist;
-
-memory_error:
-	fprintf(stderr, "error: failed to allocate memory\n");
-	exit(EXIT_FAILURE);
-}
-
-/*
-* Insert a URL into a blacklist.
-*/
-static void BlackListInsert(PBLACKLIST blacklist, PURL url)
-{
-	if (blacklist->length >= blacklist->size)
-	{
-		blacklist->size = (blacklist->size * 3) / 2;
-		printf("GROW blacklist to %u\n", blacklist->size);
-		blacklist->urls = (PURL *)realloc(blacklist->urls,
-			blacklist->size * sizeof(PURL));
-		if (blacklist->urls == NULL)
-		{
-			fprintf(stderr, "error: failed to reallocate memory\n");
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	blacklist->urls[blacklist->length++] = url;
-}
-
-/*
-* Sort the blacklist (for searching).
-*/
-static void BlackListSort(PBLACKLIST blacklist)
-{
-	qsort(blacklist->urls, blacklist->length, sizeof(PURL), UrlCompare);
-}
-
-/*
 * Match a URL against the blacklist.
 */
-static BOOL BlackListMatch(PBLACKLIST blacklist, PURL url)
+static BOOL BlackListMatch(char *domain)
 {
-	int lo = 0, hi = ((int)blacklist->length) - 1;
-
-	while (lo <= hi)
-	{
-		INT mid = (lo + hi) / 2;
-		int cmp = UrlMatch(url, blacklist->urls[mid]);
-		if (cmp > 0)
-		{
-			hi = mid - 1;
-		}
-		else if (cmp < 0)
-		{
-			lo = mid + 1;
-		}
-		else
-		{
-			return TRUE;
-		}
-	}
-	return FALSE;
+	//return True if a given URL exists in the blacklist
+	return true;
 }
-
 
 /*
 * Read URLs from a file.
 */
-static void BlackListRead(PBLACKLIST blacklist, const char *filename)
+static void BlackListRead(const char *filename)
 {
-	char domain[MAXURL + 1];
-	char uri[MAXURL + 1];
-	int c;
-	UINT16 i, j;
-	PURL url;
-	FILE *file = fopen(filename, "r");
+	FILE *fp = fopen(filename, "r");
 
-	if (file == NULL)
+	if (fp == NULL)
 	{
 		fprintf(stderr, "error: could not open blacklist file %s\n",
 			filename);
@@ -369,80 +286,12 @@ static void BlackListRead(PBLACKLIST blacklist, const char *filename)
 	}
 
 	// Read URLs from the file and add them to the blacklist: 
-	while (TRUE)
-	{
-		while (isspace(c = getc(file)))
-			;
-		if (c == EOF)
-		{
-			break;
-		}
-		if (c != '-' && !isalnum(c))
-		{
-			while (!isspace(c = getc(file)) && c != EOF)
-				;
-			if (c == EOF)
-			{
-				break;
-			}
-			continue;
-		}
-		i = 0;
-		domain[i++] = (char)c;
-		while ((isalnum(c = getc(file)) || c == '-' || c == '.') && i < MAXURL)
-		{
-			domain[i++] = (char)c;
-		}
-		domain[i] = '\0';
-		j = 0;
-		if (c == '/')
-		{
-			while (!isspace(c = getc(file)) && c != EOF && j < MAXURL)
-			{
-				uri[j++] = (char)c;
-			}
-			uri[j] = '\0';
-		}
-		else if (isspace(c))
-		{
-			uri[j] = '\0';
-		}
-		else
-		{
-			while (!isspace(c = getc(file)) && c != EOF)
-				;
-			continue;
-		}
+	fscanf(fp, "%d ", &blacklist.len);
 
-		printf("ADD %s/%s\n", domain, uri);
+	for (int i = 0; i < blacklist.len; i++)
+		fscanf(fp, "%s ", blacklist.list[i].domain);
 
-		url = (PURL)malloc(sizeof(URL));
-		if (url == NULL)
-		{
-			goto memory_error;
-		}
-		url->domain = (char *)malloc((i + 1) * sizeof(char));
-		url->uri = (char *)malloc((j + 1) * sizeof(char));
-		if (url->domain == NULL || url->uri == NULL)
-		{
-			goto memory_error;
-		}
-		strcpy(url->uri, uri);
-		for (j = 0; j < i; j++)
-		{
-			url->domain[j] = domain[i - j - 1];
-		}
-		url->domain[j] = '\0';
-
-		BlackListInsert(blacklist, url);
-	}
-
-	fclose(file);
-	return;
-
-memory_error:
-	fprintf(stderr, "error: memory allocation failed\n");
-	exit(EXIT_FAILURE);
+	fclose(fp);
 }
 
 /*
@@ -456,86 +305,46 @@ memory_error:
 *   Some browsers, such as Internet Explorer, violate these assumptions
 *   and therefore matching will not work.
 */
-static BOOL BlackListPayloadMatch(PBLACKLIST blacklist, char *data, UINT16 len)
+static BOOL BlackListPayloadMatch(char *data, UINT16 len)
 {
 	static const char get_str[] = "GET /";
 	static const char post_str[] = "POST /";
 	static const char http_host_str[] = " HTTP/1.1\r\nHost: ";
 	char domain[MAXURL];
 	char uri[MAXURL];
-	URL url = { domain, uri };
 	UINT16 i = 0, j;
 	BOOL result;
 	HANDLE console;
 
-	if (len <= sizeof(post_str) + sizeof(http_host_str))
-	{
-		return FALSE;
-	}
-	if (strncmp(data, get_str, sizeof(get_str) - 1) == 0)
-	{
-		i += sizeof(get_str) - 1;
-	}
-	else if (strncmp(data, post_str, sizeof(post_str) - 1) == 0)
-	{
-		i += sizeof(post_str) - 1;
-	}
-	else
-	{
-		return FALSE;
-	}
+	if (len <= sizeof(post_str) + sizeof(http_host_str))		 return FALSE;
+	if (strncmp(data, get_str, sizeof(get_str) - 1) == 0)		 i += sizeof(get_str) - 1;
+	else if (strncmp(data, post_str, sizeof(post_str) - 1) == 0) i += sizeof(post_str) - 1;
+	else														 return FALSE;
 
+	/* extract URI */
 	for (j = 0; i < len && data[i] != ' '; j++, i++)
-	{
 		uri[j] = data[i];
-	}
-	uri[j] = '\0';
-	if (i + sizeof(http_host_str) - 1 >= len)
-	{
-		return FALSE;
-	}
 
-	if (strncmp(data + i, http_host_str, sizeof(http_host_str) - 1) != 0)
-	{
-		return FALSE;
-	}
+	uri[j] = '\0';
+
+	if (i + sizeof(http_host_str) - 1 >= len)							  return FALSE;
+	if (strncmp(data + i, http_host_str, sizeof(http_host_str) - 1) != 0) return FALSE;
+
 	i += sizeof(http_host_str) - 1;
 
+	/* extract Domain */
 	for (j = 0; i < len && data[i] != '\r'; j++, i++)
-	{
 		domain[j] = data[i];
-	}
-	if (i >= len)
-	{
-		return FALSE;
-	}
-	if (j == 0)
-	{
-		return FALSE;
-	}
-	if (domain[j - 1] == '.')
-	{
-		// Nice try...
-		j--;
-		if (j == 0)
-		{
-			return FALSE;
-		}
-	}
+
+	if ((i >= len) || (j==0))			return FALSE;
+	if (domain[j - 1] == '.' && j==1)	return FALSE; // Nice try...
+
 	domain[j] = '\0';
 
 	printf("URL %s/%s: ", domain, uri);
 
-	// Reverse the domain:
-	for (i = 0; i < j / 2; i++)
-	{
-		char t = domain[i];
-		domain[i] = domain[j - i - 1];
-		domain[j - i - 1] = t;
-	}
-
 	// Search the blacklist:
-	result = BlackListMatch(blacklist, &url);
+	result = BlackListMatch(domain);
 
 	// Print the verdict:
 	console = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -551,55 +360,6 @@ static BOOL BlackListPayloadMatch(PBLACKLIST blacklist, char *data, UINT16 len)
 	}
 	SetConsoleTextAttribute(console,
 		FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+
 	return result;
-}
-
-/*
-* URL comparison.
-*/
-static int __cdecl UrlCompare(const void *a, const void *b)
-{
-	PURL urla = *(PURL *)a;
-	PURL urlb = *(PURL *)b;
-	int cmp = strcmp(urla->domain, urlb->domain);
-	if (cmp != 0)
-	{
-		return cmp;
-	}
-	return strcmp(urla->uri, urlb->uri);
-}
-
-/*
-* URL matching
-*/
-static int UrlMatch(PURL urla, PURL urlb)
-{
-	UINT16 i;
-
-	for (i = 0; urla->domain[i] && urlb->domain[i]; i++)
-	{
-		int cmp = (int)urlb->domain[i] - (int)urla->domain[i];
-		if (cmp != 0)
-		{
-			return cmp;
-		}
-	}
-	if (urla->domain[i] == '\0' && urlb->domain[i] != '\0')
-	{
-		return 1;
-	}
-
-	for (i = 0; urla->uri[i] && urlb->uri[i]; i++)
-	{
-		int cmp = (int)urlb->uri[i] - (int)urla->uri[i];
-		if (cmp != 0)
-		{
-			return cmp;
-		}
-	}
-	if (urla->uri[i] == '\0' && urlb->uri[i] != '\0')
-	{
-		return 1;
-	}
-	return 0;
 }
